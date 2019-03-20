38386879d1eed5b4fddf54cbd576a976b09c4e2f
bestils <thebestilspl@gmail.com>
2019-03-20 17:45:00 +0100

    poczatek pierwsza lekcja

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        int iloscPieniedzy = 2 ;
+//        double iloscPieniedzyd = 9.5 ;
+//        float  iloscPieniedzyf = 9.5f ;
+//        String  iloscPieniedzys = "siedem" ;
+//        char  iloscPieniedzyc = 'I' ;
+//        boolean  czySaPieniadze = true ;
+//
+//
+//
+//
+//
+//
+//
+//
+//        if(iloscPieniedzy>10){
+//
+//            System.out.println("Jesteś bardzo bogaty");
+//            System.out.println("Pieniadze jakie masz to " + iloscPieniedzy+ " zlotych");
+//        }
+//        else if (iloscPieniedzy>8)
+//        {
+//            System.out.println("jesteś bogaty");
+//            System.out.println("Pieniadze jakie masz to " + iloscPieniedzy+ " zlotych");
+//        }
+//        else if (iloscPieniedzy>6)
+//        {
+//            System.out.println("Jesteś średnio bogaty");
+//            System.out.println("Pieniadze jakie masz to " + iloscPieniedzy+ " zlotych");
+//        }
+//        else if (iloscPieniedzy>4)
+//        {
+//            System.out.println("Jesteś lekko biedny");
+//            System.out.println("Pieniadze jakie masz to " + iloscPieniedzy+ " zlotych");
+//        }
+//        else {
+//            for (int i=0;i<5;i++) {
+//                System.out.println("Jesteś  totalny biedakiem");
+//            }
+//            System.out.println("Pieniadze jakie masz to " + iloscPieniedzy+ " zlotych");
+//        }
+//
+//
+
+
+
+
+
+    }
+}
