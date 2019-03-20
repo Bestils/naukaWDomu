361d503b9dcab8475b90f787d5ebd8cc4f4f57a4
bestils <thebestilspl@gmail.com>
2019-03-20 18:11:45 +0100

    Drugi commit pierwsza lekcja

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        int iloscPieniedzy = 2 ;
-//        double iloscPieniedzyd = 9.5 ;
-//        float  iloscPieniedzyf = 9.5f ;
-//        String  iloscPieniedzys = "siedem" ;
-//        char  iloscPieniedzyc = 'I' ;
-//        boolean  czySaPieniadze = true ;
-//
-//
-//
-//
-//
+//        int iloscPieniedzy = 2 ; //liczba calk
+//        double iloscPieniedzyd = 9.5 ; //liczba  zmiennoprzecinkowa
+//        float  iloscPieniedzyf = 9.5f ; //liczba zmiennoprzecinkowa
+//        String  iloscPieniedzys = "siedem" ; //Napis
+//        char  iloscPieniedzyc = 'I' ; //znak
+//        boolean  czySaPieniadze = true ; //prawda/nieprawda
 //
 //
 //
@@ -45,6 +40,23 @@ public class Main {
 //
 //
 
+//        int  age =18;
+//        String imie = "Krzysiek" ;
+//        System.out.println("twoje imie to " + imie);
+//        if(age>17){
+//            System.out.println("Jesteś pełnoletni");
+//        }
+//        else {
+//            System.out.println("Jesteś dzieckiem");
+//        }
+//        for (int a=0; a<10;a++){ forma pętli for ( całkowita liczba iteracji/okrazen pętli ; do jakiego momentu ma się wykonywac ; jaki jest krok){co ma sie dziać}
+//
+//            System.out.println("Twoje okrazenie to "+ a);
+//        }
+//          if(warunek do spełnienia){ jezeli warunek spełniony to sie wykona}
+//          else if ( jezeli warunek wyżej się nie wykona sprawdz ten ){i wykonaj kod }
+//          else{jezeli sie warunki  nie spełnią to wykona się to czyli else}
+//
 
 
 
